3e9286a08e1e495cec498da7eae532518e01d1ef
soumyakarwa <soumyakarwa01@gmail.com>
2024-02-25 20:23:46 -0500

    case study structure and adding alternate images layout

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -42,17 +42,107 @@ const Content = ({ study }) => {
               ))}
             </ul>
             <div className="content-title">{study.title}</div>
-            <div className="content-subtitle">{study.subtitle}</div>
-            <div>{study.role}</div>
-            <div>{study.overview}</div>
-            <div>{study.introductionTitle}</div>
-            <div>{study.introduction}</div>
-            <div>{study.paragraph1Title}</div>
-            <div>{study.paragraph1}</div>
-            <div>{study.paragraph2Title}</div>
-            <div>{study.paragraph2}</div>
-            <div>{study.designHeading}</div>
-            <div>{study.reflectionHeading}</div>
+            {/* <div className="content-subtitle">{study.subtitle}</div> */}
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">ROLE</div>
+                <div>{study.role}</div>
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">OVERVIEW</div>
+                <div>{study.overview}</div>
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">{study.introductionTitle}</div>
+                <div>{study.introduction}</div>
+              </div>
+              <div className="images-container">
+                {study.introUrl &&
+                  study.introUrl.length > 0 &&
+                  study.introUrl.map((url, index) => (
+                    <img
+                      key={index} // Use index as a key for simplicity; consider using a unique identifier if available
+                      src={url}
+                      alt={`${study.introductionTitle} cover ${index + 1}`}
+                      className="content-image"
+                    />
+                  ))}
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">{study.paragraph1Title}</div>
+                <div>{study.paragraph1}</div>
+              </div>
+              <div className="images-container">
+                {study.para1Url &&
+                  study.para1Url.length > 0 &&
+                  study.para1Url.map((url, index) => (
+                    <img
+                      key={index} // Use index as a key for simplicity; consider using a unique identifier if available
+                      src={url}
+                      alt={`${study.pararaph1Title} cover ${index + 1}`}
+                      className="content-image"
+                    />
+                  ))}
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">{study.paragraph2Title}</div>
+                <div>{study.paragraph2}</div>
+              </div>
+              <div className="images-container">
+                {study.para2Url &&
+                  study.para2Url.length > 0 &&
+                  study.para2Url.map((url, index) => (
+                    <img
+                      key={index} // Use index as a key for simplicity; consider using a unique identifier if available
+                      src={url}
+                      alt={`${study.pararaph2Title} cover ${index + 1}`}
+                      className="content-image"
+                    />
+                  ))}
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">{study.designHeading}</div>
+              </div>
+              <div className="images-container">
+                {study.designsUrl &&
+                  study.designsUrl.length > 0 &&
+                  study.designsUrl.map((url, index) => (
+                    <img
+                      key={index} // Use index as a key for simplicity; consider using a unique identifier if available
+                      src={url}
+                      alt={`${study.designHeading} cover ${index + 1}`}
+                      className="content-image"
+                    />
+                  ))}
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-text">
+                <div className="section-heading">{study.reflectionHeading}</div>
+              </div>
+              <div className="images-container">
+                {study.reflectionUrl &&
+                  study.reflectionUrl.length > 0 &&
+                  study.reflectionUrl.map((url, index) => (
+                    <img
+                      key={index} // Use index as a key for simplicity; consider using a unique identifier if available
+                      src={url}
+                      alt={`${study.reflectionHeading} cover ${index + 1}`}
+                      className="content-image"
+                    />
+                  ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Work.css";
 import fibonacciGif from "../../assets/fibonacci/fibonacci-copy.gif";
+import fibonacciDesignUrl1 from "../../assets/fibonacci/mockup-1.PNG";
+import fibonacciDesignUrl2 from "../../assets/fibonacci/mockup-2.PNG";
+import fibonacciDesignUrl3 from "../../assets/fibonacci/mockup-3.PNG";
+import fibonacciIntro from "../../assets/fibonacci/golden-spiral.png";
+import fibonacciPara2_1 from "../../assets/fibonacci/fibonacci-1.jpg";
+import fibonacciPara2_2 from "../../assets/fibonacci/fibonacci-2.jpg";
 import lightAndTimeGif from "../../assets/lightAndTime.gif";
-import typographicFormGif from "../../assets/typographicForm.gif";
+import typographicFormGif from "../../assets/typographicForm/typographicForm.gif";
 import nuclearDawnCover from "../../assets/nuclearDawn/mockup-spread2.png";
 
 const caseStudies = [
@@ -11,23 +17,31 @@ const caseStudies = [
     title: "FIBONACCI",
     subtitle: `I created a two posters and a dynamic digital media interaction to illustrate the Fibonacci sequence, showcasing the interdependence of square sizes and their relation to the golden ratio.`,
     tags: ["Digital Media", "Poster Design"],
-    imageUrl: fibonacciGif,
     role: `Designer, Developer`,
     overview: `I created a pair of posters and an accompanying dynamic digital media interaction to illustrate the Fibonacci sequence, showcasing the interdependence of square sizes and their relation to the golden ratio.`,
-    introductionTitle: `So, what is Fibonacci?`,
+    introductionTitle: `SO, WHAT IS FIBONACCI?`,
     introduction: `The Fibonacci sequence is a series of numbers in which each number (Fibonacci number) is the sum of the two preceding ones, usually starting with 0 and 1. The sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, and so on. Interestingly, this sequence appears in many different areas of mathematics and science. It's also known for its occurrence in biological settings; for example, in branching in trees, the arrangement of leaves on a stem, the flowering of artichokes, and the arrangement of a pine cone's bracts. Apple also used the Golden Spiral to design its logo.`,
-    paragraph1Title: `Significance`,
+    paragraph1Title: `SIGNIFICANCE`,
     paragraph1: `As someone who loves math, I find the Fibonacci sequence fascinating. It's amazing how it shows up in so many places, connecting things we wouldn't think are related. It's like finding a secret code in nature that links everything together.`,
-    paragraph2Title: `Initial Sketches`,
+    paragraph2Title: `INITIAL SKETCHES`,
     paragraph2: `I wanted to focus on the recursive nature of the sequence and decided to focus on the squares.`,
-    designHeading: `Final Designs`,
-    reflectionHeading: `Reflection`,
+    designHeading: `FINAL DESIGNS`,
+    reflectionHeading: `REFLECTION`,
     reflectionContent: ``,
+    imageUrl: fibonacciGif,
+    introUrl: [fibonacciIntro],
+    para2Url: [fibonacciPara2_1, fibonacciPara2_2],
+    designsUrl: [
+      fibonacciDesignUrl1,
+      fibonacciDesignUrl2,
+      fibonacciDesignUrl3,
+      fibonacciGif,
+    ],
   },
   {
     id: 2,
     title: "TYPOGRAPHIC FORM",
-    subtitle: `I designed a dynamic typographic animation to explore the anatomy of the word "curiosity," using p5.js to create three interactive experiences: tracking mouse movement, breaking down the word into its constituent letters, and exploring each letter individually`,
+    subtitle: `I designed a dynamic typographic animation to explore the anatomy of the word "curiosity," using p5.js to create three interactive experiences: tracking mouse movement, breaking down the word into its constituent letters, and exploring each letter individually.`,
     tags: ["Digital Media"],
     imageUrl: typographicFormGif,
   },
