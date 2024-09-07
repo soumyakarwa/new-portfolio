e8f2731db1e778e3c1bc3c4f76e1802eb672a43d
soumyakarwa <soumyakarwa01@gmail.com>
2024-09-07 12:01:31 -0400

    added a nav-bar for each contents section and fixed the width of the hover in navbar

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,42 +2,73 @@
 // import React, { useEffect, useState } from "react";
 import "./Content.css";
 
-// Navigation Menu Component
-// const NavigationMenu = ({ section }) => {
-//   if (!section || !section.paragraphs) {
-//     console.error("section or paragraphs is undefined");
-//     return null;
-//   }
+const scrollWithOffset = (element) => {
+  const yOffset = -110; // Set the desired offset in pixels
+  const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
 
-//   return (
-//     <nav className="content-nav">
-//       <ul>
-//         <li key={section.role}>
-//           <a href={`#role`}>ROLE</a>
-//         </li>
-//         <li key={section.tech}>
-//           <a href={`#tech`}>TECH</a>
-//         </li>
-//         <li key={section.overview}>
-//           <a href={`#overview`}>OVERVIEW</a>
-//         </li>
-//         <li key={`section-${2}`}>
-//           <a href={`#para2`}>{section.paragraphs.para2.title}</a>
-//         </li>
-//         {Object.values(section.paragraphs).map((paragraph, index) => (
-//           <li key={`nav-para${index + 1}`}>
-//             <a href={`#para${index + 1}`}>{paragraph.title}</a>
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// };
+  window.scrollTo({ top: y, behavior: "smooth" });
+};
+
+const NavigationMenu = ({ section }) => {
+  if (!section || !section.paragraphs) {
+    console.error("section or paragraphs is undefined");
+    return null;
+  }
+
+  const handleClick = (e, id) => {
+    e.preventDefault();
+    const element = document.querySelector(id);
+    if (element) {
+      scrollWithOffset(element);
+    }
+  };
+
+  return (
+    <nav className="content-nav">
+      <ul>
+        <li key={section.role}>
+          <a href="#role" onClick={(e) => handleClick(e, "#role")}>
+            ROLE
+          </a>
+        </li>
+        <li key={section.tech}>
+          <a href="#tech" onClick={(e) => handleClick(e, "#tech")}>
+            TOOLS & TECH
+          </a>
+        </li>
+        <li key={section.overview}>
+          <a href="#overview" onClick={(e) => handleClick(e, "#overview")}>
+            OVERVIEW
+          </a>
+        </li>
+        {Object.values(section.paragraphs).map((paragraph, index) => (
+          <li key={`nav-para${index + 1}`}>
+            <a
+              href={`#para${index + 1}`}
+              onClick={(e) => handleClick(e, `#para${index + 1}`)}
+            >
+              {paragraph.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 const Content = ({ study }) => {
   return (
     <div className="content-container">
-      {/* <NavigationMenu section={study} /> */}
+      <NavigationMenu section={study} />
+
+      <div className="content-title">
+        <div>{study.title}</div>
+        <div className="myButton" id="casestudy-button">
+          <a href={study.link} target="_blank" rel="noopener noreferrer">
+            DEMO
+          </a>
+        </div>
+      </div>
 
       <div className="content">
         <div className="content-details">
@@ -52,14 +83,14 @@ const Content = ({ study }) => {
                 </li>
               ))}
             </ul> */}
-            <div className="content-title">
+            {/* <div className="content-title">
               <div>{study.title}</div>
               <div className="myButton" id="casestudy-button">
                 <a href={study.link} target="_blank" rel="noopener noreferrer">
                   DEMO
                 </a>
               </div>
-            </div>
+            </div> */}
             <div className="section" id="role">
               <div className="section-text">
                 <div className="section-heading">ROLE</div>
@@ -98,7 +129,7 @@ const Content = ({ study }) => {
                   {paragraph.imageSource &&
                     paragraph.imageSource.length > 0 &&
                     paragraph.imageSource.map((url, imgIndex) =>
-                      paragraph.imageType[index] === "iframe" ? (
+                      paragraph.imageType[imgIndex] === "iframe" ? (
                         <div
                           key={`iframe-${index}-${imgIndex}`}
                           className="iframe-container"
